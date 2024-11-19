62fa1b323b4a6f95a5abd08efa392d02243c63d6
jeyofdev <jgregoire.dev@gmail.com>
2024-11-19 14:36:38 +0100

    feat : init reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-form-inputs';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+  usernameCtrl!: FormControl<string | null>;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this._initFormControls();
+    this._initMainForm();
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  private _initMainForm() {
+    this.form = this._formBuilder.group({
+      username: this.usernameCtrl,
+    });
+  }
+
+  private _initFormControls(): void {
+    this.usernameCtrl = this._formBuilder.control('');
+  }
 }
